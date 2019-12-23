91b5b6d70ff945115ba03e6fc312f0bc958d558c
srinidhigv <srinidhi.gv42@gmail.com>
2019-12-23 09:35:17 +0530

    initial commit

---
+++ app/models/employee.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const validator=require('validator')
+
+const employeeSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        validator:{
+            validator:function(value){
+                return validator.isEmail(value)
+            },message:function(){
+                return 'invalid email format'
+            }
+        },
+        mobile:{
+            type:String,
+            unique:true,
+            validator:{
+                validator:function(value){
+                    return validator.isNumber(value)
+                },message:function(){
+                    return 'invalid mobile number'
+                }
+            },
+            department:{
+                type:Schema.Types.ObjectId,
+                required:true,
+                ref:'Department'
+            }
+        }
+    }
+})
+
+const Employee=mongoose.model('Employee',employeeSchema)
+
+module.exports=Employee
